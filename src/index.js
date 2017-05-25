eddbb9c6d218448aa8ec7f393b7f7d3cb65ade77
Wenhui Wang <w.eleventh@gmail.com>
2017-05-25 12:13:20 +0800

    Finish basic implement without Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Weather from './Weather.jsx';
+
+ReactDOM.render(<Weather />, document.getElementById('root'));
 registerServiceWorker();
